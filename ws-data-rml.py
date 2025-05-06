48dcbac43647b7322e73e5f2f2775d5f771161e5
Miguel Ceriani <miguel.ceriani@gmail.com>
2025-05-06 15:33:06 +0200

    Fix namespace usage

--- ws-data-rml.py
+++ ws-data-rml.py
@@ -18,10 +18,10 @@ from dateutil.parser import isoparse
 from urllib.parse import quote
 
 scenario_map = {
-    'rcp45': 'https://w3id.org/hacid/data/greenhousegasconcentrationpathway/rcp-4.5',
-    'rcp85': 'https://w3id.org/hacid/data/greenhousegasconcentrationpathway/rcp-8.5',
-    'rcp26': 'https://w3id.org/hacid/data/greenhousegasconcentrationpathway/rcp-2.6',
-    'rcp60': 'https://w3id.org/hacid/data/greenhousegasconcentrationpathway/rcp-6'
+    'rcp45': 'https://w3id.org/hacid/data/cs/greenhousegasconcentrationpathway/rcp-4.5',
+    'rcp85': 'https://w3id.org/hacid/data/cs/greenhousegasconcentrationpathway/rcp-8.5',
+    'rcp26': 'https://w3id.org/hacid/data/cs/greenhousegasconcentrationpathway/rcp-2.6',
+    'rcp60': 'https://w3id.org/hacid/data/cs/greenhousegasconcentrationpathway/rcp-6'
     }
     
 def _purge_none_values(dictionary: dict) -> dict:
