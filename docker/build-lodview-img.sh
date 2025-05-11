2fc6b9e1508693ea8a2879979f15294340806094
Miguel Ceriani <miguel.ceriani@gmail.com>
2025-05-11 15:06:27 +0200

    Move docker configuration to separate repo

--- docker/build-lodview-img.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d "LodView" ]; then rm -Rf LodView; fi
-git clone --branch fix-war-plugin --single-branch https://github.com/miguel76/LodView
-cd LodView
-mvn compile war:war
-docker build -t lodview .
-cd ..
