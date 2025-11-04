0b69dfb671a606dcbd36f8ef8d590a8ea320c89b
Miguel Ceriani <miguel.ceriani@gmail.com>
2025-11-04 15:30:03 +0100

    Add methdos, hazards, and cvs to mapping script

--- data-sources/mohc-cvs/map.sh
+++ data-sources/mohc-cvs/map.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 jq -f 'mapping/indices.jq' <data/indices.json >rdf/indices.jsonld
+jq -f 'mapping/cvs.jq' <data/cv-map.json >rdf/cvs.jsonld
 jq -f 'mapping/operations.jq' <data/operations.json >rdf/operations.jsonld
+jq -f 'mapping/methods.jq' <data/methods.json >rdf/methods.jsonld
+jq -f 'mapping/hazards.jq' <data/hazards.json >rdf/hazards.jsonld
