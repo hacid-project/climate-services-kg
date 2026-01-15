916b8f58f92b8ef39e8575b8861d4a59abf5dc3d
Miguel Ceriani <miguel.ceriani@gmail.com>
2026-01-15 10:22:28 +0100

    Generate whitespace as in source

--- for-hacid-dss/add-metadata.sh
+++ for-hacid-dss/add-metadata.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-jq --library-path 'mapping' -f 'add-metadata.jq' <../data-sources/mohc-cvs/rdf/operations.jsonld >output/tasks.json
+jq --indent 4 -f 'add-metadata.jq' <../data-sources/mohc-cvs/rdf/operations.jsonld >output/tasks.json
 
